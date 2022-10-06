8049f60321c1ba6be6e613297378e60f9a75bff8
Aditya kumar <yomecool@gmail.com>
2022-10-06 02:01:06 -0400

    Ak - Dataset readiness

---
+++ R Codes/IMDB_Movie_Dataset/InitialDatasetManipulation.R
@@ -0,0 +1,18 @@
+# Movie Ratings Dataset Join test
+getwd()
+
+movies <- data.frame(read.csv("movies.csv"))
+ratings <- data.frame(read.csv("ratings.csv"))
+movie_ratings <- merge(x = movies, y = ratings, by = 'movieId', all.y = TRUE)
+
+# Null checks
+max(is.na(movie_ratings))
+
+# Splitting the Genres into unique rows
+# install.packages('magrittr') - for pipe operator
+# install.packages('tidyverse') - to create unique rows
+library(magrittr)
+library(tidyverse)
+movie_ratings <- movie_ratings %>% separate_rows(genres)
+  
+
